7fcdc8cece2e180b35b96dfe49adf3ccb619be56
Christopher Farm <farm.cp@gmail.com>
2013-10-12 23:56:00 -0700

    fix shit with new controllers

---
+++ tennismatch/TennisCourtTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  TennisCourtTableViewController.h
+//  tennismatch
+//
+//  Created by Christopher Farm on 10/12/13.
+//  Copyright (c) 2013 com.ordinance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TennisCourtTableViewController : UITableViewController
+
+-(IBAction)mapViewButtonClick:(UIButton*)sender;
+
+@end
